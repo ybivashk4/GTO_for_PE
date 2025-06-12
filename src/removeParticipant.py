58dbd23a60a032222773b30d6f7e876bec1f3d0c
Tenori0n <maeestrod@gmail.com>
2025-06-13 00:05:01 +0500

    added remove participant function and fixed db

---
+++ src/removeParticipant.py
@@ -0,0 +1,12 @@
+from sqlite3 import *
+def removeParticipant(Number):
+    con = connect('../GTO.db')
+    con.execute("PRAGMA foreign_keys = ON")
+    cur = con.cursor()
+    command = f"DELETE FROM Participants WHERE ParticipantNumber = {Number}"
+    cur.execute(command)
+    con.commit()
+    cur.close()
+    con.close()
+
+removeParticipant(1)
